c8af5985e8270e25c0e7e63b12bcbd5eb3a3b5c3
Noorinder Singh <noorindersingh@edu.sait.ca>
2024-01-29 19:39:23 -0700

    Update page.js

--- app/week-2/page.js
+++ app/week-2/page.js
@@ -1,21 +1,3 @@
-// // app/page.js
-// import Link from 'next/link';
-// import StudentInfo from './student-info';
-
-// export default function Page() {
-//   return (
-//     <main>
-//       <h1>CPRG 306: Web Development</h1>
-//       <p>
-//         <Link href="/week-2" passHref>
-//           <span style={{ textDecoration: 'underline', cursor: 'pointer' }}>
-//             Week 2 Assignment (lab 2)
-//           </span>
-//         </Link>
-//       </p>
-//     </main>
-//   );
-// }
 
 // app/week-2/page.js
 import StudentInfo from './student-info';
